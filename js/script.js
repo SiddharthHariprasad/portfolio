d67ebc8b8ccd14bb1809317938293881f4c5854d
Siddharth Hariprasad <siddhuhariprasad@gmail.com>
2021-01-23 02:36:03 +0530

    First Commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Custom Script for adding functionalities
+
+$(document).ready(function(){
+ $('.sidenav').sidenav();
+ $('.materialboxed').materialbox();
+ $('.parallax').parallax();
+ $('.tabs').tabs();
+ $('.datepicker').datepicker({
+    format: 'dd mmmm, yyyy (dddd)'
+ });
+ $('.tooltipped').tooltip();
+ $('.scrollspy').scrollSpy();
+})
